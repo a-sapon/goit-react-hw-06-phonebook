f4738cb1dc6d8aac6dd26dbaedd59d7b8283237c
a-sapon <sapon.anya@gmail.com>
2020-02-21 10:42:05 +0200

    added thunk

---
+++ build/precache-manifest.a7f3d91c3439e2cc772d7ffdcf8295d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388f0bf2ef7992c3e40882b654277fbb",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "9f7701f700048c1d76d6",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.8e3d6f12.chunk.css"
+  },
+  {
+    "revision": "80f0f96e15aca7bdc9d2",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.429ae541.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.429ae541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f7701f700048c1d76d6",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.ac3599ca.chunk.js"
+  },
+  {
+    "revision": "0d4d37491f8621c9f163",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.78193737.js"
+  }
+]);
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './reducers';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk'
 
 const persistConfig = {
   key: 'root',
@@ -10,5 +11,5 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, reducer);
 
-export const store = configureStore({reducer: persistedReducer});
+export const store = configureStore({reducer: persistedReducer, middleware: [thunk]});
 export const persistor = persistStore(store);
